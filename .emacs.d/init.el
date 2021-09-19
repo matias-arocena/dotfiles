4e55dfcd8ef2707a57e7e6fbb20d405c46a53931
Matias Arocena <m.arocena97@gmail.com>
2021-09-19 19:25:37 -0300

    Add backup and autosave directory

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,24 @@
 (when (not (server-running-p))
     (server-start))
 
+;; Put backup files neatly away                                                 
+(let ((backup-dir "~/.emacs.d/backups")
+      (auto-saves-dir "~/.emacs.d/auto-saves/"))
+  (dolist (dir (list backup-dir auto-saves-dir))
+    (when (not (file-directory-p dir))
+      (make-directory dir t)))
+  (setq backup-directory-alist `(("." . ,backup-dir))
+        auto-save-file-name-transforms `((".*" ,auto-saves-dir t))
+        auto-save-list-file-prefix (concat auto-saves-dir ".saves-")
+        tramp-backup-directory-alist `((".*" . ,backup-dir))
+        tramp-auto-save-directory auto-saves-dir))
+
+(setq backup-by-copying t    ; Don't delink hardlinks                           
+      delete-old-versions t  ; Clean up the backups                             
+      version-control t      ; Use version numbers on backups,                  
+      kept-new-versions 5    ; keep some new versions                           
+      kept-old-versions 2)   ; and some old ones, too
+
 ;; Initialize package
 (require 'package)
   
@@ -334,9 +352,10 @@
 
 (use-package lsp-mode
     :commands (lsp lsp-deferred)
-    :hook (lsp-mode . mati/lsp-mode-setup)
+    :hook ((lsp-mode . mati/lsp-mode-setup)
+	   (prog-mode . lsp-mode))
     :init
-    (setq lsp-keymap-prefix "SPC l")  ;; Or 'C-l', 's-l'
+    (setq lsp-keymap-prefix "C-l")  ;; Or 'C-l', 's-l'
     :config
     (lsp-enable-which-key-integration t))
 
