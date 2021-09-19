d63b994694e897fdd121a60e0d87cead193eda1f
Matias Arocena <m.arocena97@gmail.com>
2021-09-18 22:29:06 -0300

    Use C-/ for comments

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,242 +1,245 @@
 (require 'server)
 (when (not (server-running-p))
-  (server-start))
+    (server-start))
 
-  ;; Initialize package
-  (require 'package)
+;; Initialize package
+(require 'package)
   
-  (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			   ("melpa-stable" . "https://stable.melpa.org/packages/")
-			   ("org" . "https://orgmode.org/elpa/")
-			   ("elpa" . "https://elpa.gnu.org/packages/")))
-  
-  (package-initialize)
-  (unless package-archive-contents
-    (package-refresh-contents))
-  
-  ;; Initialize use-package on non-Linux platforms
-  (unless (or (package-installed-p 'use-package))
-     (package-install 'use-package))
-  (require 'use-package)
-  
-  ;; Uncomment this to get a reading on packages that get loaded at startup
-  ;; (setq use-package-verbose t)
-  
-  (setq use-package-always-ensure t)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			("melpa-stable" . "https://stable.melpa.org/packages/")
+			("org" . "https://orgmode.org/elpa/")
+			("elpa" . "https://elpa.gnu.org/packages/")))
 
-  ;; Bootstrap straight.el
-  (defvar bootstrap-version)
-  (let ((bootstrap-file
-	(expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-	(bootstrap-version 5))
-    (unless (file-exists-p bootstrap-file)
-      (with-current-buffer
-	  (url-retrieve-synchronously
-	  "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-	  'silent 'inhibit-cookies)
-	(goto-char (point-max))
-	(eval-print-last-sexp)))
-    (load bootstrap-file nil 'nomessage))
-  
-  
-  ;; Always use straight to install on systems other than Linux
-  (setq straight-use-package-by-default (not (eq system-type 'gnu/linux)))
-  
-  ;; Use straight.el for use-package expressions
-  (straight-use-package 'use-package)
-  
-  ;; Load the helper package for commands like `straight-x-clean-unused-repos'
-  (require 'straight-x)
+(package-initialize)
+(unless package-archive-contents
+(package-refresh-contents))
 
-  ;; Thanks, but no thanks
-  (setq inhibit-startup-message t)
-  
-  (scroll-bar-mode -1)        ; Disable visible scrollbar
-  (tool-bar-mode -1)          ; Disable the toolbar
-  (tooltip-mode -1)           ; Disable tooltips
-  (set-fringe-mode 10)        ; Give some breathing room
-  
-  (menu-bar-mode -1)          ; Disable the menu bar
+;; Initialize use-package on non-Linux platforms
+(unless (or (package-installed-p 'use-package))
+    (package-install 'use-package))
+(require 'use-package)
 
-  (setq visible-bell t)
+;; Uncomment this to get a reading on packages that get loaded at startup
+;; (setq use-package-verbose t)
 
-  (setq display-line-numbers-type 'relative)
-  (global-display-line-numbers-mode t)
+(setq use-package-always-ensure t)
 
-  ;; Enable line numbers for some modes
-  (dolist (mode '(text-mode-hook
-                  prog-mode-hook
-                  conf-mode-hook))
-          (add-hook mode (lambda () (display-line-numbers-mode 1))))
+;; Bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+    (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+    (bootstrap-version 5))
+(unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	    "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	    'silent 'inhibit-cookies)
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+(load bootstrap-file nil 'nomessage))
 
-  ;; Override some modes which derive from the above
-  (dolist (mode '(org-mode-hook))
-          (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-  (pcase system-type
+;; Always use straight to install on systems other than Linux
+(setq straight-use-package-by-default (not (eq system-type 'gnu/linux)))
+
+;; Use straight.el for use-package expressions
+(straight-use-package 'use-package)
+
+;; Load the helper package for commands like `straight-x-clean-unused-repos'
+(require 'straight-x)
+
+;; Thanks, but no thanks
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+
+(menu-bar-mode -1)          ; Disable the menu bar
+
+(setq visible-bell t)
+
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode t)
+
+;; Enable line numbers for some modes
+(dolist (mode '(text-mode-hook
+		prog-mode-hook
+		conf-mode-hook))
+    (add-hook mode (lambda () (display-line-numbers-mode 1))))
+
+;; Override some modes which derive from the above
+(dolist (mode '(org-mode-hook))
+    (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(pcase system-type
     ((or 'gnu/linux 'windows-nt 'cygwin)
-     (set-face-attribute 'default nil
-			 :font "JetBrains Mono"
-			 :weight 'light
-			 :height 100)))
-  
-  (use-package doom-themes
+	(set-face-attribute 'default nil
+			:font "JetBrains Mono"
+			:weight 'light
+			:height 100)))
+
+(use-package doom-themes
     :defer t)
-  (load-theme 'doom-palenight t)
-  (doom-themes-visual-bell-config)
-  
-  (use-package rainbow-delimiters
+
+(load-theme 'doom-palenight t)
+(doom-themes-visual-bell-config)
+
+(use-package rainbow-delimiters
     :hook (emacs-lisp-mode . rainbow-delimiters-mode))
 
-  ;; Shotcut suggestions
-  (use-package which-key
+;; Shotcut suggestions
+(use-package which-key
     :init (which-key-mode)
     :diminish which-key-mode
     :config
-    (setq which-key-idle-delay 0.3))
+	(setq which-key-idle-delay 0.3))
 
 ;; Ivy
 (use-package swiper :ensure t)
 
 (use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
-         ("C-f" . ivy-alt-done)
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :init
-  (ivy-mode 1))
+    :diminish
+    :bind (("C-s" . swiper)
+	:map ivy-minibuffer-map
+	    ("TAB" . ivy-alt-done)
+	    ("C-f" . ivy-alt-done)
+	    ("C-l" . ivy-alt-done)
+	    ("C-j" . ivy-next-line)
+	    ("C-k" . ivy-previous-line)
+	:map ivy-switch-buffer-map
+	    ("C-k" . ivy-previous-line)
+	    ("C-l" . ivy-done)
+	    ("C-d" . ivy-switch-buffer-kill)
+	:map ivy-reverse-i-search-map
+	    ("C-k" . ivy-previous-line)
+	    ("C-d" . ivy-reverse-i-search-kill))
+    :init
+	(ivy-mode 1))
 
 (use-package counsel
-  :demand t
-  :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-         ("C-x C-f" . counsel-find-file)
-         ;; ("C-M-j" . counsel-switch-buffer)
-         ("C-M-l" . counsel-imenu)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history))
-  :custom
-  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
-  :config
-  (setq ivy-initial-inputs-alist nil))
+    :demand t
+    :bind (("M-x" . counsel-M-x)
+	("C-x b" . counsel-ibuffer)
+	("C-x C-f" . counsel-find-file)
+	;; ("C-M-j" . counsel-switch-buffer)
+	("C-M-l" . counsel-imenu)
+    :map minibuffer-local-map
+	("C-r" . 'counsel-minibuffer-history))
+    :custom
+	(counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
+    :config
+	(setq ivy-initial-inputs-alist nil))
 
 
 (use-package ivy-rich
-  :init
-  (ivy-rich-mode 1))
+    :init
+	(ivy-rich-mode 1))
 
 (use-package diminish
-  :ensure t)
-
+    :ensure t)
 
 (use-package doom-modeline
-  :init (doom-modeline-mode 1)) ; run M-x all-the-icons-install-fonts
+    :init (doom-modeline-mode 1)) ; run M-x all-the-icons-install-fonts
 
- (use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-symbol] . helpful-symbol)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-key] . helpful-key))
+(use-package helpful
+    :custom
+	(counsel-describe-function-function #'helpful-callable)
+	(counsel-describe-variable-function #'helpful-variable)
+    :bind
+	([remap describe-function] . counsel-describe-function)
+	([remap describe-symbol] . helpful-symbol)
+	([remap describe-variable] . counsel-describe-variable)
+	([remap describe-command] . helpful-command)
+	([remap describe-key] . helpful-key))
 
-    (use-package general
-      :config
-      (general-evil-setup t)
-    
-      (general-create-definer mati/leader-keys
-	:keymaps '(normal insert visual emacs)
-	:prefix "SPC"
-	:global-prefix "C-SPC"))
+(use-package general
+    :config
+	(general-evil-setup t)
+	(general-create-definer mati/leader-keys
+	    :keymaps '(normal insert visual emacs)
+	    :prefix "SPC"
+	    :global-prefix "C-SPC"))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mac-command-modifier 'control)
- '(mac-control-modifier nil)
- '(mac-option-modifier 'meta)
- '(mac-right-control-modifier 'left)
- '(mac-right-option-modifier 'alt))
+    ;; custom-set-variables was added by Custom.
+    ;; If you edit it by hand, you could mess it up, so be careful.
+    ;; Your init file should contain only one such instance.
+    ;; If there is more than one, they won't work right.
+    '(mac-command-modifier 'control)
+    '(mac-control-modifier nil)
+    '(mac-option-modifier 'meta)
+    '(mac-right-control-modifier 'left)
+    '(mac-right-option-modifier 'alt))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+    ;; custom-set-faces was added by Custom.
+    ;; If you edit it by hand, you could mess it up, so be careful.
+    ;; Your init file should contain only one such instance.
+    ;; If there is more than one, they won't work right.
+)
 
-  (defun mati/evil-hook ()
+(defun mati/evil-hook ()
     (dolist (mode '(custom-mode
-		    eshell-mode
-		    git-rebase-mode
-		    erc-mode
-		    circe-server-mode
-		    circe-chat-mode
-		    circe-query-mode
-		    sauron-mode
-		    term-mode))
-      (add-to-list 'evil-emacs-state-modes mode)))
-  
-  
-  (use-package undo-tree
+	    eshell-mode
+	    git-rebase-mode
+	    erc-mode
+	    circe-server-mode
+	    circe-chat-mode
+	    circe-query-mode
+	    sauron-mode
+	    term-mode))
+	(add-to-list 'evil-emacs-state-modes mode)))
+
+
+
+(use-package undo-tree			
     :init
-    (global-undo-tree-mode 1))
-  
-  (use-package evil
-    :init
+	(global-undo-tree-mode 1)
+    :bind (:map undo-tree-map
+		("C-/" . nil)
+		("C-z" . 'undo-tree-undo)
+		("C-S-z" . 'undo-tree-redo)))
+
+(use-package evil
+:init
     (setq evil-want-integration t)
     (setq evil-want-keybinding nil)
     (setq evil-want-C-u-scroll t)
     (setq evil-want-C-i-jump nil)
     (setq evil-respect-visual-line-mode t)
     (setq evil-undo-system 'undo-tree)
-    :config
+:config
     (add-hook 'evil-mode-hook 'mati/evil-hook)
     (evil-mode 1)
+
     (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
     (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-  
-    (define-key evil-normal-state-map (kbd "SPC h") help-map)
-    (define-key evil-normal-state-map (kbd "SPC x") ctl-x-map)
-    (define-key evil-normal-state-map (kbd "<SPC>c") (general-simulate-key "C-c"))
 
-    (global-set-key (kbd "C-/") 'evilnc-comment-or-uncomment-lines) 
+    (define-key evil-normal-state-map (kbd "SPC h") (general-simulate-key "C-h"))
+    (define-key evil-normal-state-map (kbd "SPC x") (general-simulate-key "C-x"))
+    (define-key evil-normal-state-map (kbd "<SPC>c") (general-simulate-key "C-c"))
 
     ;; Use visual line motions even outside of visual-line-mode buffers
     (evil-global-set-key 'motion "j" 'evil-next-visual-line)
     (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-  
+
     (evil-set-initial-state 'messages-buffer-mode 'normal)
     (evil-set-initial-state 'dashboard-mode 'normal))
-  
-  
-  (use-package evil-collection
-    :after evil
-    :config
-    (evil-collection-init))
-  
-  (use-package evil-org
-    :ensure t
-    :after org
-    :hook (org-mode . (lambda () evil-org-mode))
-    :config
-    (require 'evil-org-agenda)
-    (evil-org-agenda-set-keys))
+
+
+(use-package evil-collection
+:after evil
+:config
+(evil-collection-init))
+
+(use-package evil-org
+:ensure t
+:after org
+:hook (org-mode . (lambda () evil-org-mode))
+:config
+(require 'evil-org-agenda)
+(evil-org-agenda-set-keys))
 
     (use-package hydra)
 
@@ -293,9 +296,10 @@
 
     (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-    (global-unset-key (kbd "C-x C-b")
+    (global-unset-key (kbd "C-x C-b"))
     (global-unset-key (kbd "C-x C-@"))
     (global-unset-key (kbd "C-x C-<SPC>"))
+    (global-unset-key (kbd "C-/"))
 
     (use-package projectile
 	:diminish projectile-mode
@@ -341,19 +345,17 @@
     (use-package company
 	:after lsp-mode
 	:hook (lsp-mode . company-mode)
-	:bind (:map company-active-map
-	    ("<tab>" . company-complete-selection))
-	    (:map lsp-mode-map
-	    ("<tab>" . company-indent-or-complete-common))
-	:custoe
+	:bind (:map company-active-map ("<tab>" . company-complete-selection))
+	    (:map lsp-mode-map ("<tab>" . company-indent-or-complete-common))
+	:custom
 	(company-minimum-prefix-length 1)
 	(company-idle-delay 0.0))
 
     (use-package company-box
 	:hook (company-mode . company-box-mode))
 
-    (use-package evil-nerd-commenter
-    :bind ("M-/" . evilnc-comment-or-uncomment-lines))
+(use-package evil-nerd-commenter
+  :bind ("C-/" . evilnc-comment-or-uncomment-lines))
 
     (use-package ue
 	:straight (ue :type git :host gitlab :repo "unrealemacs/ue.el")
