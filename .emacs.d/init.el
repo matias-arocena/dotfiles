a50bb89096a783af73e118bbba03010c7b742752
Matias <matias.arocena9@protonmail.com>
2021-11-17 22:27:08 -0300

    Add PDF support

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -809,3 +809,15 @@
 ;; Telegram
 (use-package telega)
 (put 'narrow-to-region 'disabled nil)
+
+;;; PDF
+(use-package pdf-tools
+ :config
+ ;; initialise
+ (pdf-tools-install)
+ ;; open pdfs scaled to fit page
+ (setq-default pdf-view-display-size 'fit-page)
+ ;; automatically annotate highlights
+ (setq pdf-annot-activate-created-annotations t))
+ ;; use normal isearch
+ ;(define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))
